1f1052e552c42ddc77cf4983f4f51e94fee19e5f
juliancit0 <juulianconti@gmail.com>
2022-09-19 16:43:28 -0300

    BaSP-W04-2022: correct ex01 and ex-02

--- Semana-04/exercises/01-variables-and-operators.js
+++ Semana-04/exercises/01-variables-and-operators.js
@@ -4,13 +4,13 @@ console.log('-- EXERCISE 1: VARIABLES AND OPERATORS');
  en una 3er variable. */
 
 console.log ('--Exercise 1.a')
-var firstNumber=1, secondNumber=2, result;
+var firstNumber = 1, secondNumber = 2, result;
 result = firstNumber + secondNumber;
 console.log (result);
 
 /* b. Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable. */
 
-console.log ('/n --Exercise 1.b')
+console.log ('\n --Exercise 1.b')
 var firstString='Hello ', secondString='world!', result;
 result = firstString + secondString;
 console.log(result)
@@ -18,6 +18,7 @@ console.log(result)
 /* Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string)
 +guardando el resultado de la suma en una 3er variable (utilizar length). */
 
-var totalCharacters;
+console.log ('\n --Exercise 1.c')
+var firstString='Hello ', secondString='world!', totalCharacters;
 totalCharacters = firstString.length + secondString.length;
 console.log (totalCharacters);
--- Semana-04/exercises/02-strings.js
+++ Semana-04/exercises/02-strings.js
@@ -1,15 +1,19 @@
 console.log('-- EXERCISE 2: STRINGS');
 
-/* Crear una variable de tipo string con al menos 10 caracteres y convertir todo el texto en mayúscula
-(utilizar toUpperCase). */
+/*
+a. Crear una variable de tipo string con al menos 10 caracteres y convertir todo el texto en mayúscula
+(utilizar toUpperCase).
+*/
 
 console.log ('--Exercise 2.a')
 var firstString = 'Hello World!';
 firstString = firstString.toUpperCase();
 console.log(firstString);
 
-/* Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los primeros
- 5 caracteres guardando el resultado en una nueva variable (utilizar substring).*/
+/*
+b. Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los primeros
+5 caracteres guardando el resultado en una nueva variable (utilizar substring).
+*/
 
 console.log ('\n--Exercise 2.b')
 var secondString = 'Example text', result;
@@ -17,10 +21,47 @@ result = secondString.substring(0,5);
 console.log(result);
 
 
-/* Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los últimos 3
- caracteres guardando el resultado en una nueva variable (utilizar substring). */
+/*
+c. Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los últimos 3
+caracteres guardando el resultado en una nueva variable (utilizar substring).
+*/
 
 console.log ('\n--Exercise 2.c')
 var thirdString = 'Another example text';
 result = thirdString.substring(thirdString.length-3);
 console.log(result)
+
+/*
+d.Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con la primera
+letra en mayúscula y las demás en minúscula. Guardar el resultado en una nueva variable (utilizar substring,
+toUpperCase, toLowerCase y el operador +).
+*/
+
+console.log ('\n--Exercise 2.d')
+var fourthString = 'aNOTHER EXAMPLE TEXT', result;
+result =  fourthString.substring(0,1).toUpperCase() + fourthString.substring(1).toLocaleLowerCase();
+console.log(result)
+
+/*
+e.Crear una variable de tipo string con al menos 10 caracteres y algún espacio en blanco. Encontrar la
+posición del primer espacio en blanco y guardarla en una variable (utilizar indexOf).
+*/
+
+console.log ('\n--Exercise 2.e')
+var fifthString = secondString, result;
+result = fifthString.indexOf(' ');
+console.log(result)
+
+/*
+f. Crear una variable de tipo string con al menos 2 palabras largas (10 caracteres y algún espacio entre medio).
+Utilizar los métodos de los ejercicios anteriores para generar un nuevo string que tenga la primera letra de
+ambas palabras en mayúscula y las demás letras en minúscula (utilizar indexOf, substring, toUpperCase,
+toLowerCase y el operador +).
+*/
+
+console.log ('\n--Exercise 2.f')
+var sixthString = 'aNoThEr eXaMPLE', spacePosition = sixthString.indexOf(' '), result;
+result = sixthString[0].toUpperCase() + sixthString.substring(1, spacePosition + 1).toLowerCase() +
+sixthString[spacePosition+1].toUpperCase() + sixthString.substring(spacePosition+2).toLowerCase();
+console.log(result)
+
